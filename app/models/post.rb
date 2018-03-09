334006ffebbed59fed37dc400b1a859b41c6a2a8
Yomaira Escano <yjescano@gmail.com>
2018-03-09 19:56:38 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,18 +8,8 @@ class Post < ActiveRecord::Base
   CLICKBAIT_VALUES = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
 
   def clickbait
-
     if CLICKBAIT_VALUES.none? {|bait| bait.match title}
       errors.add(:title, "Needs to be clickbait!")
     end
-    # if
-    #   CLICKBAIT_VALUES.each do |bait|
-    #   bait.match :title
-    #   end
-    #   errors.add(:title, "Needs to be clickbait!")
-    # end
-    # if CLICKBAIT_VALUES.none? {|cb| cb.match title}
-    #   errors.add(:title, "Must be clickbait")
-    # end
   end
 end
