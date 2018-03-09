d536e9e3c03d9e4f9c27f3c599bdbe4ffe051367
Yomaira Escano <yjescano@gmail.com>
2018-03-09 20:00:23 +0000

    automatically backed up by learn

--- app/helpers/my_validator.rb
+++
@@ -1,8 +0,0 @@
-class MyValidator < ActiveModel::Validator
-  def validate(record, number)
-    unless record.title.include?
-      "Won't Believe" || "Secret" || "Top #{number}" || "Guess"
-      record.errors[:title] << "Title is not valid."
-    end
-  end
-end
